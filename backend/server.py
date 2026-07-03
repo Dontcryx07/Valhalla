f7d6f80a5acb1e9049f62c5c38a42f87ba40f89d
Dontcryx_07 <parv210727@gmail.com>
2026-07-03 10:46:12 +0530

    frontend Server v1

--- backend/server.py
+++ backend/server.py
@@ -97,4 +97,4 @@ async def websocket_endpoint(websocket: WebSocket):
 app.mount("/", StaticFiles(directory=FRONTEND, html=True), name="frontend")
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=True)
